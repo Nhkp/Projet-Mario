589a67cd229e87f7d52f68e67e231e611d66f7f1
Nhkp <palezis.c@gmail.com>
2020-02-27 20:02:41 +0100

    debug saut

--- Sources/src/mario.c
+++ Sources/src/mario.c
@@ -10,28 +10,25 @@
 extern int mv_background[];
 dynamic_object_t mario;
 
-int update(int dt){
-  return (int)(-(dt*dt/10)+8*dt);
+int update(int dt)
+{
+  return (int)(-(dt * dt / 10) + 8 * dt);
 }
 
 int tmp = -1;
 void animation_mario_moves(dynamic_object_t *obj, int up, int down, int left, int right, int space)
 {
-  
 
-
-  obj->xs = 5;
-  obj->ys = 15;
+  obj->xs = 4;
+  obj->ys = 16;
   int test = map_get(obj->x / 64, (obj->y / 64)+2);
-
-
  
   static int dt;
   static int jumping;
   static int saving;
   if (up && !obj->state) 
   {
-    obj->y -= 24;
+    obj->y -= obj->ys;
     dt = 0;
 
     saving = obj->y;
@@ -47,42 +44,35 @@ void animation_mario_moves(dynamic_object_t *obj, int up, int down, int left, in
     dt ++;}
   }
   
-if (test)
-{
-  obj->state = OBJECT_STATE_NORMAL;
-}
+  if (test)
+    obj->state = OBJECT_STATE_NORMAL;
 
   // gravité
-  if (tab[test].type == MAP_OBJECT_AIR || !test)
-  {
-    obj->y += obj->ys;    
-  }
+  if (!collision(obj, DOWN))
+    obj->y += obj->ys;
 
-
-  
 
   //LEFT
-  if(left && !collision(obj, LEFT))
+  if (left && !collision(obj, LEFT))
   {
     obj->direction = 1;
     obj->x -= obj->xs;
 
-    obj->anim_next_step = (obj->anim_next_step < obj->sprite->nb_step-1) ? obj->anim_next_step+1 : 0; 
+    obj->anim_next_step = (obj->anim_next_step < obj->sprite->nb_step - 1) ? obj->anim_next_step + 1 : 0;
   }
   else if (left)
-    obj->anim_next_step = (obj->anim_next_step < obj->sprite->nb_step-1) ? obj->anim_next_step+1 : 0; 
+    obj->anim_next_step = (obj->anim_next_step < obj->sprite->nb_step - 1) ? obj->anim_next_step + 1 : 0;
 
   //RIGHT
-  if(right && !collision(obj, RIGHT))
+  if (right && !collision(obj, RIGHT))
   {
     obj->direction = 0;
     obj->x += obj->xs;
-    
-    obj->anim_next_step = (obj->anim_next_step < obj->sprite->nb_step-1) ? obj->anim_next_step+1 : 0; 
+
+    obj->anim_next_step = (obj->anim_next_step < obj->sprite->nb_step - 1) ? obj->anim_next_step + 1 : 0;
   }
   else if (right)
-    obj->anim_next_step = (obj->anim_next_step < obj->sprite->nb_step-1) ? obj->anim_next_step+1 : 0; 
-
+    obj->anim_next_step = (obj->anim_next_step < obj->sprite->nb_step - 1) ? obj->anim_next_step + 1 : 0;
 }
 
 void animation_mario_one_step(dynamic_object_t *obj)
