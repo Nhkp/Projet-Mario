e24e9c6cfb6710c6557a5fd36bc88c1cf176f00c
Nhkp <palezis.c@gmail.com>
2020-03-01 17:26:21 +0100

    réaction en chaine TNT

--- Sources/src/map_objects.c
+++ Sources/src/map_objects.c
@@ -12,35 +12,91 @@ void tnt_explode(int x, int y)
 
     //int id = timer_set(500, &add_explosion2);
     add_explosion2(x * TILE - TILE, y * TILE - TILE);
-    if (map_get(x - 1, y - 1))
-        printf("%d\n",map_get(x - 1, y - 1));
-        map_set(0, x - 1, y - 1);
-
+    switch(map_get(x - 1, y - 1))
+    {
+        case 6:
+            tnt_explode(x - 1, y - 1);
+            map_set(0, x - 1, y - 1);
+            break;
+        default:
+            map_set(0, x - 1, y - 1);
+            break;
+    }
     add_explosion2(x * TILE, y * TILE - TILE);
-    if (map_get(x , y - 1))
-        map_set(0, x, y - 1);
-
+    switch(map_get(x , y - 1))
+    {
+        case 6:
+            tnt_explode(x, y - 1);
+            map_set(0, x, y - 1);
+            break;
+        default:
+            map_set(0, x, y - 1);
+            break;
+    }
     add_explosion2(x * TILE + TILE, y * TILE - TILE);
-    if (map_get(x + 1, y - 1))
-        map_set(0, x + 1, y - 1);
-
+    switch(map_get(x + 1, y - 1))
+    {
+        case 6:
+            tnt_explode(x + 1, y - 1);
+            map_set(0, x + 1, y - 1);
+            break;
+        default:
+            map_set(0, x + 1, y - 1);
+            break;
+    }
     add_explosion2(x * TILE + TILE, y * TILE);
-    if (map_get(x + 1, y))
-        map_set(0, x + 1, y);
-
+    switch(map_get(x + 1, y))
+    {
+        case 6:
+            tnt_explode(x + 1, y);
+            map_set(0, x + 1, y);
+            break;
+        default:
+            map_set(0, x + 1, y);
+            break;
+    }
     add_explosion2(x * TILE + TILE, y * TILE + TILE);
-    if (map_get(x + 1, y + 1))
-        map_set(0, x + 1, y + 1);
-
+    switch(map_get(x + 1, y + 1))
+    {
+        case 6:
+            tnt_explode(x + 1, y + 1);
+            map_set(0, x + 1, y + 1);
+            break;
+        default:
+            map_set(0, x + 1, y + 1);
+            break;
+    }
     add_explosion2(x * TILE, y * TILE + TILE);
-    if (map_get(x, y + 1))
-        map_set(0, x, y + 1);
-
+    switch(map_get(x, y + 1))
+    {
+        case 6:
+            tnt_explode(x, y + 1);
+            map_set(0, x, y + 1);
+            break;
+        default:
+            map_set(0, x, y + 1);
+            break;
+    }
     add_explosion2(x * TILE - TILE, y * TILE + TILE);
-    if (map_get(x - 1, y + 1))
-        map_set(0, x - 1, y + 1);
-
+    switch(map_get(x - 1, y + 1))
+    {
+        case 6:
+            tnt_explode(x - 1, y + 1);
+            map_set(0, x - 1, y + 1);
+            break;
+        default:
+            map_set(0, x - 1, y + 1);
+            break;
+    }
     add_explosion2(x * TILE - TILE, y * TILE);
-    if (map_get(x - 1, y))
-        map_set(0, x - 1, y);
+    switch(map_get(x - 1, y))
+    {
+        case 6:
+            tnt_explode(x - 1, y);
+            map_set(0, x - 1, y);
+            break;
+        default:
+            map_set(0, x - 1, y);
+            break;
+    }
 }
