1fd14b9f9266e2becd8d8c84ac4449df095450bb
Clement Palezis <Clement.Palezis@cea.fr>
2022-05-12 09:04:54 +0200

    build: add a colored summary and a sound enabling option

---
+++ cmake/colors.cmake
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: XXX
+# Copyright (C) 2021 XXX
+
+string(ASCII 27 Esc)
+set(COLOR_ColorReset  "${Esc}[m")
+set(COLOR_ColorBold   "${Esc}[1m")
+set(COLOR_Red         "${Esc}[31m")
+set(COLOR_Green       "${Esc}[32m")
+set(COLOR_Yellow      "${Esc}[33m")
+set(COLOR_Brown       "${Esc}[0;33m")
+set(COLOR_Blue        "${Esc}[34m")
+set(COLOR_Magenta     "${Esc}[35m")
+set(COLOR_Cyan        "${Esc}[36m")
+set(COLOR_White       "${Esc}[37m")
+set(COLOR_BoldGrey    "${Esc}[1;30m")
+set(COLOR_BoldRed     "${Esc}[1;31m")
+set(COLOR_BoldGreen   "${Esc}[1;32m")
+set(COLOR_BoldYellow  "${Esc}[1;33m")
+set(COLOR_BoldBlue    "${Esc}[1;34m")
+set(COLOR_BoldMagenta "${Esc}[1;35m")
+set(COLOR_BoldCyan    "${Esc}[1;36m")
+set(COLOR_BoldWhite   "${Esc}[1;37m")
